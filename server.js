65a8f2d398311f667605c770d4184c115d42f6a5
varnit raina <varnitraina@gmail.com>
2025-08-18 01:20:57 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path'); 
+const { Server } = require('socket.io');
+const ACTIONS = require('./src/Actions');
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const userSocketMap = {};
+const roomCodeMap = {};
+
+function getAllConnectedClients(roomId) {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(socketId => ({
+    socketId,
+    username: userSocketMap[socketId],
+  }));
+}
+
+io.on('connection', socket => {
+  console.log('Socket connected', socket.id);
+
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+
+    const clients = getAllConnectedClients(roomId);
+
+    io.to(roomId).emit(ACTIONS.JOINED, {
+      clients,
+      username,
+      socketId: socket.id,
+    });
+
+    if (roomCodeMap[roomId]) {
+      socket.emit(ACTIONS.CODE_CHANGE, { code: roomCodeMap[roomId] });
+    }
+  });
+
+  socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+    console.log('receiving', code);
+    roomCodeMap[roomId] = code;
+    socket.to(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
+
+  socket.on('disconnecting', () => {
+    const username = userSocketMap[socket.id];
+    const rooms = [...socket.rooms];
+
+    rooms.forEach(roomId => {
+      socket.to(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username,
+      });
+
+      // Emit a separate event to update the client list for the remaining users.
+      // This is a more robust way to handle state synchronization.
+      setTimeout(() => {
+        const clients = getAllConnectedClients(roomId);
+        io.to(roomId).emit(ACTIONS.JOINED, { clients });
+      }, 100);
+    });
+
+    delete userSocketMap[socket.id];
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
